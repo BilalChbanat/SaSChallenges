84402b7d20c4f07c8e03fec0102821e67d171483
BilalChbanat <bilal.chbanat2003@gmail.com>
2023-09-14 13:32:25 +0100

    challenge condition

--- Les_Condition/challenge10/convertdate.c
+++ Les_Condition/challenge10/convertdate.c
@@ -5,9 +5,8 @@ int main(int argc, char const *argv[])
 //12 dyal ch7al man fasila kayna 
 //20 dyal ch7al man character kayn wasst fassila
 {
-    char days[12][20]= {"janvier", "février", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "décembre"};
+    char moins[12][20]= {"janvier", "février", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "décembre"};
 
- 
     int d,m,y;
 
 //sscanf
@@ -23,40 +22,40 @@ int main(int argc, char const *argv[])
 
     switch(m){
         case 1:
-            printf( "%d-%s-%d",d ,days[0],d);
+            printf( "%d-%s-%d",d ,moins[0],d);
             break;
         case 2:
-            printf( "%d-%s-%d",d ,days[1],d);
+            printf( "%d-%s-%d",d ,moins[1],d);
             break;
         case 3:
-            printf( "%d-%s-%d",d ,days[2],d);
+            printf( "%d-%s-%d",d ,moins[2],d);
             break;
         case 4:
-            printf( "%d-%s-%d",d ,days[3],d);
+            printf( "%d-%s-%d",d ,moins[3],d);
             break;
         case 5:
-            printf( "%d-%s-%d",d ,days[4],d);
+            printf( "%d-%s-%d",d ,moins[4],d);
             break;
         case 6:
-            printf( "%d-%s-%d",d ,days[5],d);
+            printf( "%d-%s-%d",d ,moins[5],d);
             break;
         case 7:
-            printf( "%d-%s-%d",d ,days[6],d);
+            printf( "%d-%s-%d",d ,moins[6],d);
             break;
         case 8:
-            printf( "%d-%s-%d",d ,days[7],d);
+            printf( "%d-%s-%d",d ,moins[7],d);
             break;
         case 9:
-            printf( "%d-%s-%d",d ,days[8],d);
+            printf( "%d-%s-%d",d ,moins[8],d);
             break;
         case 10:
-            printf( "%d-%s-%d",d ,days[9],d);
+            printf( "%d-%s-%d",d ,moins[9],d);
             break;
         case 11:
-            printf( "%d-%s-%d",d ,days[10],d);
+            printf( "%d-%s-%d",d ,moins[10],d);
             break;
         case 12:
-            printf( "%d-%s-%d",d ,days[11],d);
+            printf( "%d-%s-%d",d ,moins[11],d);
             break;
         default:
             printf("invalide moins");
---
+++ Les_Condition/challenge11/random.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+       char days[12][20]= {"lundi", "mardi", "mercredi"," jeudi", "vendredi", "samedi "," dimanche"};
+    return 0;
+}
